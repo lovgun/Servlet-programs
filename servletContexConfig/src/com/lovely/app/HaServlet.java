11a72be5018b0b92e110b168bf63d22c47b05b9c
lovgun <lovgun96@gmail.com>
2024-02-28 13:56:39 +0530

    "this program describe servelt config and servlet context concept"

---
+++ servletContexConfig/src/com/lovely/app/HaServlet.java
@@ -0,0 +1,26 @@
+package com.lovely.app;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HaServlet extends HttpServlet{
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		res.setContentType("text/html");
+		PrintWriter writer=res.getWriter();
+		ServletContext context=getServletContext();
+		String offer=context.getInitParameter("offer");
+		writer.print(offer);
+		ServletConfig config=getServletConfig();
+		String discount=config.getInitParameter("discount");
+		writer.print("<br> HouseAppliences discount" +discount);
+	}
+
+}
